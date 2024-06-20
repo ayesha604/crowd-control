d5608089f2ef0a8474e7a4e4d624219f905b4b56
ayesha604 <halim.aye64@gmail.com>
2024-06-19 20:58:29 -0700

    higher accuracy achieved

--- src/Map.js
+++ src/Map.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import { Box, Typography, Button, IconButton, Stack } from '@mui/material';
 
 const containerStyle = {
-    width: '70vw',
+    width: '65vw',
     height: '70vh',
 };
 
@@ -14,8 +14,8 @@ const defaultCenter = {
 
 export default function Map() {
     const [userLocation, setUserLocation] = useState(null);
-
-    useEffect(() => {
+    
+    const fetchUserLocation = useCallback(() => {
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(
                 (position) => {
@@ -24,10 +24,22 @@ export default function Map() {
                 },
                 (error) => {
                     console.error("Error getting user's location: ", error);
+                    setUserLocation(defaultCenter);
+                },
+                {
+                    enableHighAccuracy: true,
+                    timeout: 5000,
+                    maximumAge: 0,
                 }
-            );
+            ); 
+        } else {
+            setUserLocation(defaultCenter);
         }
     }, []);
+
+    useEffect(() => {
+        fetchUserLocation();
+    }, [fetchUserLocation]);
 
     return (
     //   <Box className="App" sx={{minHeight: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
